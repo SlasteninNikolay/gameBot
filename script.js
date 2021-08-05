ddc76e76f9372068d55da7dece67a767bcc46d8d
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-06 01:06:28 +0300

    Добавил счетчик попыток

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let isNumber = function (n) {
 // Игра
 function game() {
     let botNum = Math.ceil(Math.random() * 100);
+    let attempts = 10;
     return function gameBot() {
         let userNum = prompt("Угадай число от 1 до 100");
         if (userNum === null) {
@@ -18,10 +19,16 @@ function game() {
         } else if (+userNum === botNum) {
             return alert("Поздравляю, Вы угадали!!!");
         } else if (+userNum < botNum) {
-            alert("Загаданное число больше");
+            attempts = attempts - 1;
+            alert(
+                "Загаданное число больше, осталось попыток " + String(attempts)
+            );
             return gameBot();
         } else if (+userNum > botNum) {
-            alert("Загаданное число меньше");
+            attempts = attempts - 1;
+            alert(
+                "Загаданное число меньше, осталось попыток " + String(attempts)
+            );
             return gameBot();
         }
     };
