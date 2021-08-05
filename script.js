a5a32def58eef0c3a4bf3b2cf69e7703f4818fc2
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-06 00:52:14 +0300

    Удалил лишние комментарии

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 "use strict";
 
-// let userNum = +prompt("Введи число!");
-// function gameBot(n) {
-//     return function (x) {
-//         if (n === x) {
-//             return alert("Угадал!");
-//         } else if (n === 0) {
-//             return alert("Игра окончена");
-//         } else if (n > x) {
-//             alert("Загаданное число больше");
-//         } else if (n < x) {
-//             console.log(n);
-//             return alert("Загаданное число меньше");
-//         }
-//     };
-// }
-
-// const start = gameBot(userNum);
-// console.log(start(Math.ceil(Math.random() * 100)));
-
 // Функция проверки ввода числа
 let isNumber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+// Игра
 function game() {
     let botNum = Math.ceil(Math.random() * 100);
     return function gameBot() {
