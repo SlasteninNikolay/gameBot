6dd2eb22a2645e8cf261d1869dded37691ab31f7
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-06 00:46:28 +0300

    Исправил ошибки

--- script.js
+++ script.js
@@ -19,14 +19,22 @@
 // const start = gameBot(userNum);
 // console.log(start(Math.ceil(Math.random() * 100)));
 
+// Функция проверки ввода числа
+let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 function game() {
     let botNum = Math.ceil(Math.random() * 100);
     return function gameBot() {
         let userNum = prompt("Угадай число от 1 до 100");
-        if (+userNum === botNum) {
-            return alert("Поздравляю, Вы угадали!!!");
-        } else if (userNum === null) {
+        if (userNum === null) {
             return alert("Игра окончена");
+        } else if (!isNumber(userNum)) {
+            alert("Введите число!");
+            return gameBot();
+        } else if (+userNum === botNum) {
+            return alert("Поздравляю, Вы угадали!!!");
         } else if (+userNum < botNum) {
             alert("Загаданное число больше");
             return gameBot();
