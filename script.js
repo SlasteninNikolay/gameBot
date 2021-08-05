627d8718354e855a76cb01144e6ceed22bd93f5e
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-06 02:45:49 +0300

    Добавил попытки, перезапуск, проверку кол-в попыток

--- script.js
+++ script.js
@@ -9,22 +9,47 @@ let isNumber = function (n) {
 function game() {
     let botNum = Math.ceil(Math.random() * 100);
     let attempts = 10;
+
     return function gameBot() {
+        // Проверка кол-ва оставшихся попыток
+        if (attempts <= 0) {
+            let flag = confirm("Попытки закончились, хотите сыграть еще?");
+            if (flag === true) {
+                return location.reload();
+            } else {
+                return alert("Досвидания!");
+            }
+        }
+
+        // Логика бота
         let userNum = prompt("Угадай число от 1 до 100");
         if (userNum === null) {
+            //если нажали отмену
             return alert("Игра окончена");
         } else if (!isNumber(userNum)) {
+            //если введено не число
             alert("Введите число!");
             return gameBot();
         } else if (+userNum === botNum) {
-            return alert("Поздравляю, Вы угадали!!!");
+            //если число угадано
+            let flag = confirm(
+                "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
+            );
+            if (flag === true) {
+                //перезапуск или выход
+                return location.reload();
+            } else {
+                return alert("Досвидания!");
+            }
         } else if (+userNum < botNum) {
+            //если число больше
             attempts = attempts - 1;
             alert(
                 "Загаданное число больше, осталось попыток " + String(attempts)
             );
             return gameBot();
         } else if (+userNum > botNum) {
+            //если чило меньше
             attempts = attempts - 1;
             alert(
                 "Загаданное число меньше, осталось попыток " + String(attempts)
@@ -33,6 +58,7 @@ function game() {
         }
     };
 }
-
+// Запуск игры
 let start = game();
+console.dir(start);
 start();
